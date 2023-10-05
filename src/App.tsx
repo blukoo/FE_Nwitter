a0f58d4e3eab5d4d71d972152cb8a073ee6adbc3
blukoo <dnehdrkd123@naver.com>
2023-10-05 21:51:32 +0900

    [feat]로그인 이슈수정
    로그인 안되면 뒤로

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Suspense, useEffect } from "react";
+import { Suspense, useCallback, useEffect } from "react";
 import { AuthContextProvider } from "@/contexts/AuthContext";
 import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
 import { MutateContextProvider } from "@/contexts/MutateContext";
@@ -7,19 +7,43 @@ import Layout from "@/components/Layout";
 import "@/styles/App.scss";
 import UtilsContextProvider from "@/contexts/UtilsContext";
 import { PopupPortal } from "@/components/Popup/PopupPortal";
-import { ModalPopupContextProvider } from "@/contexts/ModalPopupContext";
+import {
+  ModalPopupContextProvider,
+} from "@/contexts/ModalPopupContext";
+import { Popup as PopupEnum } from "@/enum/Popup";
 import Popup from "@/components/Popup";
-const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      suspense: true
-    }
-  }
-});
+import { useNavigate } from "react-router-dom";
 function App() {
-  useEffect(()=>{
-    document.getElementsByTagName("body")[0].style.height=window.innerHeight+"px"
-  },[])
+  const navigate = useNavigate();
+  const handleError = useCallback(e => {
+    alert(JSON.stringify(e));
+    if (e.message.includes("Authentication")) {
+      // setPopup("로그인 부터 해주시기 바랍니다", {
+      // [PopupEnum.Confirm]: () =>
+
+      if (localStorage.getItem("token")) {
+        localStorage.removeItem("userInfo");
+        localStorage.removeItem("token");
+      } else if (sessionStorage.getItem("token")) {
+        sessionStorage.removeItem("userInfo");
+        sessionStorage.removeItem("token");
+      }
+      navigate("/home");
+      // });
+    }
+  }, []);
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+        suspense: true,
+        onError: e => handleError(e)
+      }
+    }
+  });
+  useEffect(() => {
+    document.getElementsByTagName("body")[0].style.height =
+      window.innerHeight + "px";
+  }, []);
   return (
     <Suspense fallback={<div>로딩중이에요!!!!</div>}>
       <QueryClientProvider client={queryClient}>
