68654165b35c4d0ed34b34e15a95f0ede8b464af
blukoo <dnehdrkd123@naver.com>
2023-12-30 22:44:55 +0900

    [fix]protectRoute수정

--- src/components/ProtectedRoute.tsx
+++ src/components/ProtectedRoute.tsx
@@ -1,19 +1,17 @@
 import { UseAuthContext, AuthContextProvider } from "@/contexts/AuthContext";
 import { UseModalPopupContext } from "@/contexts/ModalPopupContext";
 import { useNavigate } from "react-router-dom";
-import { useEffect } from "react";
+import { useEffect, useLayoutEffect } from "react";
 import { Popup } from "@/enum/Popup";
 export default function ProtectedRoute({ children }: { children }) {
   const { setPopup } = UseModalPopupContext();
   const { isLogin } = UseAuthContext();
   const navigate = useNavigate();
-  useEffect(() => {
-    // if (!isLogin) {
-    if(!localStorage.getItem("token") && !sessionStorage.getItem("token")){
-      setPopup("로그인 부터 해주시기 바랍니다", {
-        Confirm: () => navigate("/")
-      });
-    }
-  }, []);
+  if(!localStorage.getItem("token") && !sessionStorage.getItem("token")){
+    setPopup("로그인 부터 해주시기 바랍니다", {
+      Confirm: () => navigate("/")
+    });
+    return
+  }
   return children;
 }
